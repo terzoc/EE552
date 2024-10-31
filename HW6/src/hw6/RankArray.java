143582d21c3389c659f92fc3e812f5c48aabe754
terzoc <cterzo001@gmail.com>
2024-10-31 17:14:06 -0400

    started hw6

---
+++ HW6/src/hw6/RankArray.java
@@ -0,0 +1,14 @@
+package hw6;
+
+public class RankArray {
+	
+	public int[] arrayRankTransform(int[] arr) {
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/src/test/thing.java
@@ -0,0 +1,15 @@
+package test;
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.lang.Math;
+
+public class thing {
+	
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5,6};
+		System.out.println(nums[1] + nums[4] + "M" + nums[nums.length-2]);
+	}
+		
+
+}
